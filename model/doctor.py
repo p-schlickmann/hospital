67b177841667774c09777a5378f7e77722fe6d5b
tales-mendes <72951751+tales-mendes@users.noreply.github.com>
2021-08-12 22:36:44 -0300

    getters and setters added, "available" attribute added

--- model/doctor.py
+++ model/doctor.py
@@ -2,5 +2,35 @@ from model.person import Person
 
 
 class Doctor(Person):
-    def __init__(self, name, phone_number, cpf, date_of_birth, salary, on_call):
-        pass
+    def __init__(self, name =  str, phone_number: int, cpf: int, date_of_birth: datetime, salary: int, on_call: bool, available = bool):
+        self.__name = name
+        self.__phone_number = phone_number
+        self.__cpf = cpf
+        self.__ date_of_birth
+        self.__salary = salary
+        self.__on_call = on_call
+        self.__available = available
+        
+    @property
+    def salary(self):
+        return self.__salary
+    
+    @salary.setter
+    def salary(self, salary: int):
+        self.__salary = salary
+        
+    @property
+    def on_call(self):
+        return self.__on_call
+    
+    @on_call.setter
+    def on_call(self. on_call: bool):
+        self.__on_call = on_call
+        
+    @property
+    def available(self):
+        return self.__available
+    
+    @available.setter
+    def available(self, available: bool):
+        self.__available = available
