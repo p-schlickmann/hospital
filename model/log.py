d53c9c27eb4e8adb3c2da34c8c2444b959b49720
tales-mendes <72951751+tales-mendes@users.noreply.github.com>
2021-08-15 17:11:27 -0300

    getters and setters added

--- model/log.py
+++ model/log.py
@@ -1,3 +1,38 @@
 class Log:
-    def __init__(self, id, title, description, happened_at):
-        pass
+    def __init__(self, id: int, title: str, description: str, happened_at: datetime):
+        self.__id = id
+        self.__title = title
+        self.__description = description
+        self.__happened_at = happened_at
+        
+    @property
+    def id(self):
+        return self.__id
+    
+    @id.setter
+    def id(self, id: int):
+        self.__id = id
+        
+    @property
+    def title(self):
+        return self.__title
+    
+    @title.setter
+    def title(self, title: str):
+        self.__title = title
+        
+    @property
+    defdescription(self):
+        return self.__description
+    
+    @description.setter
+    def description(self, description: str):
+        self.__description = description
+        
+    @property
+    def happened_at(self):
+        return self.__happened_at
+    
+    @happened_at.setter
+    def happened_at(self, happened_at: datetime):
+        self.__happened_at = happened_at
