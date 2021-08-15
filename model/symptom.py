64b69c4d291b59f1d2840d385354de145c3041a0
tales-mendes <72951751+tales-mendes@users.noreply.github.com>
2021-08-15 13:51:01 -0300

    getters and setters added

--- model/symptom.py
+++ model/symptom.py
@@ -1,3 +1,38 @@
 class Symptom:
-    def __init__(self, name, description, discomfort_level, id):
-        pass
+    def __init__(self, name: str, description: str, discomfort_level: int, id: int):
+        self.__name = name
+        self.__description+ description
+        self.__discomfort_level = discomfort_level
+        self.__id = id
+        
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, name: str):
+        self.__name = name
+        
+    @property
+    def description(self):
+        return self.__description
+    
+    @description.setter
+    def description(self, description: str):
+        self.__description = description
+        
+    @property
+    def discomfort_level(self):
+        return self.__discomfort_level
+    
+    @discomfort_level.setter
+    def discomfort_level(self, discomfort_level: int):
+        self.__discomfort_level = discomfort_level
+        
+    @property
+    def id(self):
+        return self.__id
+    
+    @id.setter
+    def id(self, id: int):
+        self.__id = id
