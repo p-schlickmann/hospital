e99d99350053de20a6bd5011ca9bbbce152efd63
tales-mendes <72951751+tales-mendes@users.noreply.github.com>
2021-08-15 11:24:32 -0300

    getters and setters added

--- model/illness.py
+++ model/illness.py
@@ -1,3 +1,38 @@
 class Illness:
-    def __init__(self, name, description, severity, cured=False):
-        pass
+    def __init__(self, name: str, description: str, severity: int, cured=False):
+        self.__name = name
+        self.__description = description
+        self.__severity = severity
+        self.__cured = cured
+        
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, name: str):
+        self.__name = name
+        
+    @property
+    def description(self):
+        return self.__description
+    
+    @description.setter
+    def description(self, description: str):
+        self.__description = description
+        
+    @property
+    def severity(self):
+        return self.__severity
+    
+    @severity.setter
+    def severity(self, severity: int):
+        self.__severity = severity
+        
+    @property
+    def cured(self):
+        return self.__cured
+    
+    @cured.setter
+    def cured(self, cured: bool):
+        self.__cured = cured
